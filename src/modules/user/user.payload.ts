d0e7ac5cfbcea7448cf43f6d26d26a6d396a159f
Santiago <sammysan007@gmail.com>
2023-05-13 18:17:17 -0300

    refactor(user): added new user fields

--- src/modules/user/user.payload.ts
+++ src/modules/user/user.payload.ts
@@ -5,4 +5,14 @@ export class UserPayload extends Document {
   public email: string;
   public imageUrl: string;
   public password: string;
+  public celular: string;
+  public cep: string;
+  public endereco: string;
+  public numero: string;
+  public complemento: string;
+  public bairro: string;
+  public cidade: string;
+  public uf: string;
+  public empresa: string;
+  public cnpj: string;
 }
